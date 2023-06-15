c48761f97399910d8e3d3bab976dd356e9ebe72d
licask8 <elielson.elrick@gmail.com>
2023-06-15 17:16:07 -0300

    ajustes navegação mobile

--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -3,18 +3,18 @@ import React from 'react';
 // import data
 import { nav } from '../data';
 
-const NavMobile = ({ navMobile }) => {
+const NavMobile = ({ navMobile, setNavMobile }) => {
   return (
     <nav
       className={`${
         navMobile ? 'min-h-screen' : 'min-h-0'
       } w-full fixed top-0 left-0 right-0 bg-neutral-500 -bottom-12 -z-10 lg:hidden overflow-hidden transition-all h-0`}
     >
-      <ul className='w-full top-0 left-0 h-full flex flex-col justify-center items-center gap-y-8'>
+      <ul className='w-full top-0 left-0 h-full flex flex-col justify-center items-center gap-y-8' onClick={() => setNavMobile(!navMobile)}>
         {nav.map((item, idx) => {
           return (
-            <li key={idx}>
-              <a className='text-white text-body-md' href={item.href}>
+            <li key={idx} >
+              <a className='text-white text-body-md' href={item.href} >
                 {item.name}
               </a>
             </li>
